99db2acc58f69f650ad2f50d62ee5af842cbe84a
zhanghailong <zhanghailong@bitnei.cn>
2018-11-16 14:40:46 +0800

    spring boot actuator

---
+++ src/main/java/com/zhl/springactuator/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.zhl.springactuator.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping(value = "/hello")
+    public String index(){
+        return "Hello!!!";
+    }
+}
